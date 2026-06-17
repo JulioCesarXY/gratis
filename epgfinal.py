a4e796c7d8e2e1bddaeb6e2c5b7ab150953ca142
Julio Cesar <104462712+JulioCesarXY@users.noreply.github.com>
2026-06-16 21:21:09 -0300

    Add new EPG source for latinasat

--- epgfinal.py
+++ epgfinal.py
@@ -18,7 +18,8 @@ EPG_SOURCES = [
     "https://raw.githubusercontent.com/JulioCesarXY/EPG-Plex-Brazil/refs/heads/main/plex_epg_brasil.xml",
     "https://raw.githubusercontent.com/JulioCesarXY/gerador-de-epg/refs/heads/main/yeeaahtv_epg.xml",
     "https://pastebin.com/raw/PkLp1BhD",
-"https://raw.githubusercontent.com/JulioCesarXY/gerador-de-epg/refs/heads/main/epg_canal_vazio.xml"
+"https://raw.githubusercontent.com/JulioCesarXY/gerador-de-epg/refs/heads/main/epg_canal_vazio.xml",
+    "https://raw.githubusercontent.com/JulioCesarXY/gerador-de-epg/refs/heads/main/latinasat_epg.xml"
 
 ]
 
